c2f01add64c8ed1e158dd80cd03619f68d1aeda5
ozlima <ozielima001@gmail.com>
2026-06-05 21:56:59 -0300

    Force setup UI when installer is run

--- IDV-Tracker-Installer/IDV-Tracker.bat
+++ IDV-Tracker-Installer/IDV-Tracker.bat
@@ -29,7 +29,7 @@ if exist "%DIR%\.env" (
     powershell -NoProfile -ExecutionPolicy Bypass -Command "$envText = 'SUPABASE_URL=' + $env:SUPABASE_URL + \"`r`n\" + 'SUPABASE_ANON_KEY=' + $env:SUPABASE_ANON_KEY; Set-Content -LiteralPath '%DIR%\.env' -Value $envText -Encoding UTF8"
 )
 
-if exist "%DIR%\.installed" if exist "%DIR%\node_modules" (
+if not "%IDV_FORCE_SETUP%"=="1" if exist "%DIR%\.installed" if exist "%DIR%\node_modules" (
     start "IDV Tracker" /MIN cmd /c ""%BOOTSTRAP%" --run"
     exit /b
 )
--- IDV-Tracker-Setup.cmd
+++ IDV-Tracker-Setup.cmd
@@ -80,6 +80,7 @@ if errorlevel 1 (
 
 call :log "Iniciando instalador visual..."
 set "IDV_SETUP_LOG=%SETUP_LOG%"
+set "IDV_FORCE_SETUP=1"
 call "%BOOTSTRAP%" >> "%SETUP_LOG%" 2>&1
 if errorlevel 1 (
     call :fail "O instalador retornou erro. Mande o setup.log."
